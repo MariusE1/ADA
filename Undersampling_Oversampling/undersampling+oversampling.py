6684d506726ec6f6a58c6990662013488f2c4d3d
pcleres <p.cleres@gmail.com>
2020-05-24 17:33:28 +0200

    added oversampling

--- Undersampling_Oversampling/undersampling+oversampling.py
+++ Undersampling_Oversampling/undersampling+oversampling.py
@@ -12,14 +12,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
 
-###############################################################################################################################################################################################################
-#Code start
-###############################################################################################################################################################################################################
-
 os.system("clear")
 
 dataset = pd.read_csv("creditcard.csv")
-
 scaler = StandardScaler()
 
 #this adds two rows at the end of the dataset containing the scaled values of the amount and time column 
@@ -44,16 +39,19 @@ dataset.insert(1,"scaled_time",scaled_time)
 
 
 #We know that there is 492 fraud cases in the dataset from the description. We need to take all the fraud rows and take 492 random genuine rows.
+#Check the count of the 2 classes
+print(dataset["Class"].value_counts())
+
 
 #First we can shuffle the whole data set with pandas
-
 shuffled_dataset = dataset.sample(frac=1) #samples all rows without replacement, the frac argument specifies the fraction of rows to return in the random sample, with frac=1 the whole dataset is returned in a random order
 
-fraud_data = shuffled_dataset.loc[shuffled_dataset["Class"] == 1]
-genuine_data = shuffled_dataset.loc[shuffled_dataset["Class"] == 0][:492]
+fraud_data = shuffled_dataset.loc[shuffled_dataset["Class"] == 1] #used for undersampling and oversampling
+genuine_data = shuffled_dataset.loc[shuffled_dataset["Class"] == 0] #will be used for oversampling later
+genuine_data_cut = shuffled_dataset.loc[shuffled_dataset["Class"] == 0][:492] #used for undersampling
 
 #now we put together in one dataframe both fraud and genuine data
-undersampled_dataset = pd.concat([fraud_data,genuine_data])
+undersampled_dataset = pd.concat([fraud_data,genuine_data_cut])
 print("Not shuffled undersampled dataset")
 print(undersampled_dataset)
 
@@ -115,4 +113,32 @@ cb = plt.colorbar()
 cb.ax.tick_params(labelsize=14)
 plt.title("Undersampled dataset correlation matrix", fontsize=16);
 plt.savefig("Undersampled_matplotlib_correlation_matrix.png")
+plt.show()
+
+###############################################################################################################################################################################################################
+# Random Over sampling
+###############################################################################################################################################################################################################
+
+print("\nthe computation of the oversampled dataset takes some time...\n")
+
+#we need to recall that we already have arrays containing all fraud data and all genuine data. For our oversampled dataset, we will simply duplicate the fraud data 500 times and shuffle the dataset again.
+
+count = 0
+while count<500 :
+    if count == 0:  
+        oversampled_dataset = pd.concat([fraud_data,genuine_data])
+        count += 1
+    else:
+        oversampled_dataset = pd.concat([oversampled_dataset,fraud_data])
+        count += 1
+        
+print(oversampled_dataset["Class"].value_counts()) #this allows us to see the new balance between both classes.
+shuffled_oversampled_dataset = oversampled_dataset.sample(frac=1) #shuffles the new dataset again
+
+f = plt.figure(figsize=(15,11))
+oversampled_corr = shuffled_oversampled_dataset.corr()
+cmap = sn.diverging_palette(150, 4, s=80, n=100)
+sn.heatmap(oversampled_corr, cmap=cmap, annot_kws={"size":20})
+plt.title("Oversampled dataset correlation matrix", fontsize=16);
+plt.savefig("Oversampled_seaborn_correlation_matrix.png")
 plt.show()
