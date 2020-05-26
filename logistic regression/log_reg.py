332be76fd2f179b0e7b0622609036236a35815f8
MariusE1 <38375295+MariusE1@users.noreply.github.com>
2020-05-26 03:07:33 +0200

    Update log_reg.py

--- logistic regression/log_reg.py
+++ logistic regression/log_reg.py
@@ -16,3 +16,28 @@ from sklearn.model_selection import cross_val_predict
 
 log_reg_pred = cross_val_predict(log_reg, X_train, y_train, cv=5,
                              method="decision_function")
+
+
+# Final Score in the test set of logistic regression
+from sklearn.metrics import accuracy_score
+
+# Logistic Regression with Under-Sampling
+y_pred = log_reg.predict(X_test)
+undersample_score = accuracy_score(y_test, y_pred)
+
+
+# Logistic Regression with SMOTE Technique // best technique for highest result
+y_pred_sm = best_est.predict(original_Xtest)
+oversample_score = accuracy_score(original_ytest, y_pred_sm)
+
+
+d = {'Technique': ['Random UnderSampling', 'Oversampling (SMOTE)'], 'Score': [undersample_score, oversample_score]}
+final_df = pd.DataFrame(data=d)
+
+# Move column
+score = final_df['Score']
+final_df.drop('Score', axis=1, inplace=True)
+final_df.insert(1, 'Score', score)
+
+# Note how high is accuracy score it can be misleading! 
+final_df
